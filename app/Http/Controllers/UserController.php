8df1536ad0e84170c98d955da89b220c501fa3f3
Unknown <geofrey.muinde@strathmore.edu>
2018-06-25 20:36:42 +0300

    ff

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -36,7 +36,7 @@ class UserController extends Controller
 
 		DB::table('user_94231s')->insert($data);
  
-		//return response()->json($data);
+		return response()->json($data);
 	}
 
 	public function showgymusers()
