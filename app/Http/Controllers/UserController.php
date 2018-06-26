f221bb61aa15791cdae2ea437a956543d994e3e1
Unknown <geofrey.muinde@strathmore.edu>
2018-06-26 14:35:55 +0300

    bbb

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use DB;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Auth;
+//use Illuminate\Support\Facades\Auth;
 use App\User_94231;
 use app\User;
 //use Illuminate\Support\Facades\Crypt;
