80881e191b16e8ff653e301f2348c2140b6d64e9
Unknown <geofrey.muinde@strathmore.edu>
2018-06-26 14:30:51 +0300

    bb

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Auth;
 use App\User_94231;
 use app\User;
 //use Illuminate\Support\Facades\Crypt;
-use Crypt;
+//use Crypt;
 
 class UserController extends Controller
 {
